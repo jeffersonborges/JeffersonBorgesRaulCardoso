9ea34d1422886bfcee62100de1b65b899b02d2fa
Usuário Convidado <Guest@pa06micro05.local>
2018-04-23 21:49:35 -0300

    Base local

---
+++ valorReal/ItemRegisterViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ItemRegisterViewController.swift
+//  valorReal
+//
+//  Created by Usuário Convidado on 23/04/18.
+//  Copyright © 2018 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class ItemRegisterViewController: UIViewController {
+
+    @IBOutlet weak var nomeProduto: UITextField!
+    @IBOutlet weak var imgProduto: UIImageView!
+    @IBOutlet weak var ufProduto: UITextField!
+    @IBOutlet weak var valorProduto: UITextField!
+    @IBOutlet weak var swCartao: UISwitch!
+    
+    // MARK: - Properties
+    //var movie: Movie!
+    var smallImage: UIImage!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addImageProduto(_ sender: Any) {
+        //Criando o alerta que será apresentado ao usuário
+        let alert = UIAlertController(title: "Selecionar poster", message: "De onde você quer escolher o poster?", preferredStyle: .actionSheet)
+        
+        //Verificamos se o device possui câmera. Se sim, adicionamos a devida UIAlertAction
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action: UIAlertAction) in
+                self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        
+        //As UIAlertActions de Biblioteca de fotos e Álbum de fotos também são criadas e adicionadas
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let photosAction = UIAlertAction(title: "Álbum de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .savedPhotosAlbum)
+        }
+        alert.addAction(photosAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK:  Methods
+    func selectPicture(sourceType: UIImagePickerControllerSourceType) {
+        //Criando o objeto UIImagePickerController
+        let imagePicker = UIImagePickerController()
+        
+        //Definimos seu sourceType através do parâmetro passado
+        imagePicker.sourceType = sourceType
+        
+        //Definimos a MovieRegisterViewController como sendo a delegate do imagePicker
+        imagePicker.delegate = self
+        
+        //Apresentamos a imagePicker ao usuário
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ItemRegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //O método abaixo nos trará a imagem selecionada pelo usuário em seu tamanho original
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String: AnyObject]?) {
+        
+        //Iremos usar o código abaixo para criar uma versão reduzida da imagem escolhida pelo usuário
+        let smallSize = CGSize(width: 300, height: 280)
+        UIGraphicsBeginImageContext(smallSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+        
+        //Atribuímos a versão reduzida da imagem à variável smallImage
+        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        imgProduto.image = smallImage //Atribuindo a imagem à ivPoster
+        
+        //Aqui efetuamos o dismiss na UIImagePickerController, para retornar à tela anterior
+        dismiss(animated: true, completion: nil)
+    }
+}
